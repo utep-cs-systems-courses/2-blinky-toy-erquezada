d0cc7e8e574fb88ceed32e083125220444ec937b
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-03-23 00:05:26 -0600

    Delete project directory

--- project/blinky-toy/lightStateMachine.h
+++
@@ -1,7 +0,0 @@
-#ifndef lightStateMachine_included
-#define lightStateMachine_included
-
-void advance_alternation();
-void reset_alternation();
-
-#endif
--- project/blinky-toy/songStateMachine.h
+++
@@ -1,7 +0,0 @@
-#ifndef metalCrusherStateMachine_included
-#define metalCrusherStateMachine_included
-
-void advance_song();
-void reset_song();
-
-#endif
